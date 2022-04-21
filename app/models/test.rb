fe9075dfd2ed6c25038cd5879f7e9097e9e8d6ca
Maksim133 <pravo24kr2008@gmail.com>
2022-04-21 22:48:39 +0700

    fix: according to the mentor 's comments

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   def self.sorting_an_array(category)
     joins('JOIN categories ON categories.id = tests.category_id')
-    .where('categories.title = :category', category: category)
-    .order('tests.title DESC')
-    .pluck('tests.title')
+    .where(categories: {title: category})
+    .order(title: :desc)
+    .pluck(:title)
   end
 end
