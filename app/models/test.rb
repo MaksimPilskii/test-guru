97af04bfe6a526ff4b76644f020bb5e168277620
Maksim133 <pravo24kr2008@gmail.com>
2022-04-24 15:28:49 +0700

    fix: syntax errors

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,8 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :question
   belongs_to :user
-  has_many :user, through :testuser
-  
+  has_many :user, through:gi :testuser
+
   def self.sorting_an_array(category)
     joins('JOIN categories ON categories.id = tests.category_id')
     .where(categories: {title: category})
