a518bf6c6bdfe385c7495896d77f8d1850b08418
Maksim133 <pravo24kr2008@gmail.com>
2022-05-16 17:18:35 +0700

    fix: corrected according to the comments of the mentor

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
-  before_action :find_question, only: %i[show]
+  before_action :find_question, only: %i[show destroy]
   before_action :find_test, only: %i[new create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
@@ -31,21 +31,21 @@ class QuestionsController < ApplicationController
     render plain: 'Вопрос был удален'
   end
 
-  def find_question
-    @question = Question.find(params[:id])
-  end
+  private
 
   def find_test
     @test = Test.find(params[:test_id])
   end
 
-  private
-
-  def question_params
-    params.require(:question).permit(:body)
+  def find_question
+    @question = Question.find(params[:id])
   end
 
   def rescue_with_question_not_found
     render plain: 'Question was not found'
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
   end
 end
